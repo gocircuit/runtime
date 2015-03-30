9320c38e2b20182e5acb964e85f46de5c5fbac1d
Petar Maymounkov <petarm@gmail.com>
2015-03-30 15:42:49 -0400

    comment

--- sys/peer.go
+++ sys/peer.go
@@ -9,7 +9,7 @@ package sys
 
 const Scheme = "circuit"
 
-// Addr represents the identity of a unique remote worker.
+// Addr represents a locator for a unique remote worker.
 // The implementing type must be registered with package encoding/gob.
 type Addr interface {
 
@@ -20,21 +20,20 @@ type Addr interface {
 	Id() Id
 }
 
-// Conn is a connection to a remote endpoint.
+// Conn is a connection to a remote endpoint, identified by an Addr.
 type Conn interface {
 
-	// The language runtime does not itself utilize timeouts on read/write
-	// operations. Instead, it requires that calls to Read and Write be blocking
+	// The language runtime does not itself utilize timeouts on send/receive
+	// operations. Instead, it requires that calls to Send and Receive be blocking
 	// until success or irrecoverable failure is reached.
 	//
 	// The implementation of Conn must monitor the liveness of the remote
 	// endpoint using an out-of-band (non-visible to the runtime) method. If
-	// the endpoint is considered dead, all pending Read and Write request must
+	// the endpoint is considered dead, all pending Send and Receive request must
 	// return with non-nil error.
 	//
-	// A non-nil error returned on any invokation of Read and Write signals to
-	// the runtime that not just the connection, but the entire runtime
-	// (identified by its address) behind the connection is dead.
+	// A non-nil error returned on any invokation of Send and Receive signals to
+	// the runtime that the connection is dead.
 	//
 	// Such an event triggers various language runtime actions such as, for
 	// example, releasing all values exported to that runtime. Therefore, a
@@ -44,13 +43,13 @@ type Conn interface {
 	// that the runtime has no semantic limits on the length of blocking waits.
 	// In fact, the runtime has no notion of time altogether.
 
-	// Read/Write operations must panic on any encoding/decoding errors.
+	// Send/Receive operations must panic on any encoding/decoding errors.
 	// Whereas they must return an error for any exernal (network) unexpected
-	// conditions.  Encoding errors indicate compile-time errors (that will be
+	// conditions. Encoding errors indicate compile-time errors (which would be
 	// caught automatically once the system has its own compiler) but might be
 	// missed by the bare Go compiler.
 	//
-	// Read/Write must be re-entrant.
+	// Send/Receive must be re-entrant.
 
 	// Receive reads the next value from the connection.
 	Receive() (interface{}, error)
@@ -87,10 +86,3 @@ type Peer interface {
 	Dialer
 	Listener
 }
-
-// System creates a new transport framework for the given local address
-// type System interface {
-// 	NewPeer(workerId Id, addr net.Addr, key []byte) Peer
-// 	ParseNetAddr(s string) (net.Addr, error)
-// 	ParseAddr(s string) (Addr, error)
-// }
